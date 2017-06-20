7267ac8407f261c55da29d2955093de68a787e18
limozaza <zakboite@gmail.com>
2017-06-20 18:44:07 +0200

    reading duration video

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //Node supporte pas import from :(
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow }  = electron;
+const { app, BrowserWindow, ipcMain }  = electron;
 
 app.on('ready',()=>{
   //creation d'une fenetre
   const mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path,(err, metadata) =>  {
+      console.log(metadata.format.duration);
+  });
+
 });
